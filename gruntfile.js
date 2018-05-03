73909bbc53fce8585297271db8583a331abded60
Baptiste Lhopitault <baptisel@pictarine.com>
2018-05-03 10:58:38 +0200

    MAJ packages

--- gruntfile.js
+++ gruntfile.js
@@ -1,12 +1,10 @@
 module.exports = function(grunt){
 
 	//lancement de toutes les tâches sans avoir à les lister
-    require('load-grunt-tasks')(grunt);
+  require('load-grunt-tasks')(grunt);
 
 	//création des tâches
 	grunt.initConfig({	//initialisation de l'ensemble des tâches
-
-
          sass: {
             dev: {
                     files: {
@@ -22,9 +20,9 @@ module.exports = function(grunt){
             sample: {
                 src: [ 'dev/data.json' ]
             }
-        },
+          },
 
-         jade: {
+         pug: {
             compile: {
                options: {
                    client: false,
@@ -32,7 +30,7 @@ module.exports = function(grunt){
                    data: grunt.file.readJSON("dev/data.json")
                },
                files: {
-                 'dev/index.html': 'dev/jade/index.jade'
+                 'dev/index.html': 'dev/pug/index.pug'
                }
           }
         },
@@ -57,7 +55,7 @@ module.exports = function(grunt){
     	watch: {
             sass : {
                 files: ['dev/sass/**/*.scss'],
-                tasks: ['jade','sass:dev'],
+                tasks: ['pug','sass:dev'],
                 options: { spawn: false, livereload: true }
             },
 
@@ -68,15 +66,15 @@ module.exports = function(grunt){
 
             json: {
                 files: ['dev/data.json'],
-                tasks: ['jsonlint','jade'],
+                tasks: ['jsonlint','pug'],
                 options: {livereload: true}
 
             },
 
-            jade: {
+            pug: {
                 options: {livereload: true},
-                files: 'dev/**/*.jade',
-                tasks: ['jade']
+                files: 'dev/**/*.pug',
+                tasks: ['pug']
             }
         }
 
@@ -85,7 +83,7 @@ module.exports = function(grunt){
 
     //lanceur de tâche
 
-        //Dev - convertisseur jade->HTML et livereload
+        //Dev - convertisseur pug->HTML et livereload
         grunt.registerTask(
             'default',
             ['watch']
@@ -94,7 +92,7 @@ module.exports = function(grunt){
         //Inliner
         grunt.registerTask(
             'deploy',
-            ['jade','sass:dev','inlinecss']
+            ['pug','sass:dev','inlinecss']
         );
 
 
